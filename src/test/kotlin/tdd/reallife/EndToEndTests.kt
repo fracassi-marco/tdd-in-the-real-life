6a404da35d9823cc45aa2bbd4ac2f8a91efa26d8
Marco Fracassi <fracassi.marco@gmail.com>
2021-03-05 17:12:01 +0100

    Add E2E test

---
+++ src/test/kotlin/tdd/reallife/EndToEndTests.kt
@@ -0,0 +1,62 @@
+package tdd.reallife
+
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.openqa.selenium.By.className
+import org.openqa.selenium.By.id
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
+
+class EndToEndTests {
+
+    private var app = App()
+    private lateinit var browser: WebDriver
+
+    @BeforeEach
+    fun beforeEach() {
+        WebDriverManager.firefoxdriver().setup()
+        browser = FirefoxDriver(FirefoxOptions().setHeadless(true))
+        app.start()
+        cleanCart()
+        insertProduct("Potato")
+    }
+
+    @AfterEach
+    fun afterEach() {
+        browser.close()
+        app.close()
+        deleteProduct("Potato")
+    }
+
+    @Test
+    fun payByCash() {
+        browser.get("http://localhost:4545/")
+        browser.findElement(id("button-Potato")).click()
+        assertThat(browser.findElements(className("cart-item")).single().text).isEqualTo("Potato")
+
+        browser.findElement(id("button-cash")).click()
+        assertThat(browser.findElements(className("cart-item"))).isEmpty()
+    }
+
+    private fun cleanCart() {
+        DbConnection().create().use {
+            it.prepareStatement("TRUNCATE TABLE cart").execute()
+        }
+    }
+
+    private fun insertProduct(name: String) {
+        DbConnection().create().use {
+            it.prepareStatement("INSERT INTO products VALUES ('$name', 'very good', 'http://any.img')").execute()
+        }
+    }
+
+    private fun deleteProduct(name: String) {
+        DbConnection().create().use {
+            it.prepareStatement("DELETE FROM products WHERE name = '$name'").execute()
+        }
+    }
+}
